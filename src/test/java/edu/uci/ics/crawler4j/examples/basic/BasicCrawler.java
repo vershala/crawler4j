00f353338fa4fdc44e828a2ccbec13bc941b9bcc
Yasser Ganjisaffar <yasserg@users.noreply.github.com>
2017-01-31 09:22:32 -0800

    11111

--- src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawler.java
+++ src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawler.java
@@ -17,6 +17,13 @@
 
 package edu.uci.ics.crawler4j.examples.basic;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Set;
 import java.util.regex.Pattern;
 
@@ -32,65 +39,54 @@ import edu.uci.ics.crawler4j.url.WebURL;
  */
 public class BasicCrawler extends WebCrawler {
 
-    private static final Pattern IMAGE_EXTENSIONS = Pattern.compile(".*\\.(bmp|gif|jpg|png)$");
+	private static final Pattern IMAGE_EXTENSIONS = Pattern.compile(".*\\.(bmp|css|js)$");
 
-    /**
-     * You should implement this function to specify whether the given url
-     * should be crawled or not (based on your crawling logic).
-     */
-    @Override
-    public boolean shouldVisit(Page referringPage, WebURL url) {
-        String href = url.getURL().toLowerCase();
-        // Ignore the url if it has an extension that matches our defined set of image extensions.
-        if (IMAGE_EXTENSIONS.matcher(href).matches()) {
-            return false;
-        }
+	/**
+	 * You should implement this function to specify whether the given url
+	 * should be crawled or not (based on your crawling logic).
+	 */
+	@Override
+	public boolean shouldVisit(Page referringPage, WebURL url) {
+		String href = url.getURL().toLowerCase();
+		// Ignore the url if it has an extension that matches our defined set of
+		// image extensions.
+		if (IMAGE_EXTENSIONS.matcher(href).matches()) {
+			return false;
+		}
 
-        // Only accept the url if it is in the "www.ics.uci.edu" domain and protocol is "http".
-        return href.startsWith("http://www.ics.uci.edu/");
-    }
+		// Only accept the url if it is in the "10.70.149.88:8082/scm" domain
+		// and protocol is "http".
+		return href.startsWith("http://www.nipic.com/");
+	}
 
-    /**
-     * This function is called when a page is fetched and ready to be processed
-     * by your program.
-     */
-    @Override
-    public void visit(Page page) {
-        int docid = page.getWebURL().getDocid();
-        String url = page.getWebURL().getURL();
-        String domain = page.getWebURL().getDomain();
-        String path = page.getWebURL().getPath();
-        String subDomain = page.getWebURL().getSubDomain();
-        String parentUrl = page.getWebURL().getParentUrl();
-        String anchor = page.getWebURL().getAnchor();
+	/**
+	 * This function is called when a page is fetched and ready to be processed
+	 * by your program.
+	 */
+	@Override
+	public void visit(Page page) {
+		String url = page.getWebURL().getURL();
+		logger.info("URL: {}", url);
 
-        logger.debug("Docid: {}", docid);
-        logger.info("URL: {}", url);
-        logger.debug("Domain: '{}'", domain);
-        logger.debug("Sub-domain: '{}'", subDomain);
-        logger.debug("Path: '{}'", path);
-        logger.debug("Parent page: {}", parentUrl);
-        logger.debug("Anchor text: {}", anchor);
+		if (page.getParseData() instanceof HtmlParseData) {
+			HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+			String text = htmlParseData.getText();
+			String html = htmlParseData.getHtml();
+			Set<WebURL> links = htmlParseData.getOutgoingUrls();
+			logger.debug("Text length: {}", text.length());
+			logger.debug("Html length: {}", html.length());
+			logger.debug("Number of outgoing links: {}", links.size());
+		}
 
-        if (page.getParseData() instanceof HtmlParseData) {
-            HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
-            String text = htmlParseData.getText();
-            String html = htmlParseData.getHtml();
-            Set<WebURL> links = htmlParseData.getOutgoingUrls();
+		Header[] responseHeaders = page.getFetchResponseHeaders();
+		if (responseHeaders != null) {
+			logger.debug("Response headers:");
+			for (Header header : responseHeaders) {
+				logger.debug("\t{}: {}", header.getName(), header.getValue());
+			}
+		}
 
-            logger.debug("Text length: {}", text.length());
-            logger.debug("Html length: {}", html.length());
-            logger.debug("Number of outgoing links: {}", links.size());
-        }
-
-        Header[] responseHeaders = page.getFetchResponseHeaders();
-        if (responseHeaders != null) {
-            logger.debug("Response headers:");
-            for (Header header : responseHeaders) {
-                logger.debug("\t{}: {}", header.getName(), header.getValue());
-            }
-        }
-
-        logger.debug("=============");
-    }
+		logger.debug("=============");
+	}
+	
 }
